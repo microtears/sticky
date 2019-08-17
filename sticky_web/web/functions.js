2d04ed8fab5647c5134459794b9d54dfdebd8120
microtears <microtears@gmail.com>
2019-08-18 02:50:40 +0800

    xhr

---
+++ sticky_web/web/functions.js
@@ -0,0 +1,44 @@
+function http({
+    url,
+    method = "get",
+}) {
+    // Return a new promise.
+    return new Promise(function (resolve, reject) {
+        // Do the usual XHR stuff
+        var req = new XMLHttpRequest();
+        req.open(method || 'GET', url);
+        req.onload = function () {
+            // This is called even on 404 etc
+            // so check the status
+            if (req.status == 200) {
+                // Resolve the promise with the response text
+                resolve(req.response);
+            }
+            else {
+                // Otherwise reject with the status text
+                // which will hopefully be a meaningful error
+                reject(Error(req.statusText));
+            }
+        };
+
+        // Handle network errors
+        req.onerror = function () {
+            reject(Error("Network Error"));
+        };
+
+        // Make the request
+        req.send();
+    });
+}
+
+function post(url) {
+    return http({ url: url, method: "POST" });
+}
+
+function get(url) {
+    return http({ url: url, method: "GET" });
+}
+
+function getJson(url) {
+    return get(url).then(JSON.parse);
+}
